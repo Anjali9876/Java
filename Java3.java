cbc0c5fe49ac0b3cf123161a09b7629ab88487a8
Anjali9876 <am.votum@gmail.com>
2019-07-07 21:54:05 +0530

    Third problem

--- Java3.java
+++ Java3.java
@@ -1,25 +1,18 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
 
-
-
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        if(N%2==0)
-            System.out.println("Not Weird");
-        else
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 2 == 1) {
             System.out.println("Weird");
-        scanner.close();
+        } else if (n % 2 == 0 && n >= 2 && n <= 5) {
+            System.out.println("Not Weird");
+        } else if (n % 2 == 0 && n >= 6 && n <= 20) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
     }
 }
-
